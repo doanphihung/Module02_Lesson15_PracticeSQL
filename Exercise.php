4602228c1473fa36ba1f6c3d409926b397112fce
doanphihung <79822736+doanphihung@users.noreply.github.com>
2021-04-25 10:24:47 +0700

    lesson15
    thuc hanh ngon ngu truy van SQL

---
+++ Exercise.php
@@ -0,0 +1,7 @@
+<?php
+/*
+1. SELECT * FROM customer;
+2. SELECT customerName, phone, city, country FROM customers;
+3. SELECT * FROM customers WHERE customername like '%A%';
+4. SELECT * FROM customers WHERE city IN ('Nantes',' Las Vegas',' Warszawa','NYC');
+*/
